eaed951da41698770cc55c82e3b80f431258fc33
Rene Ballesteros Machdo Junior <renebmjr@gmail.com>
2025-03-30 16:12:36 -0300

    pack00002 - Criacao de preferences.py e algumas correcoes e melhorias em app_editor_pixelart.py criacao do schema 7

--- db_migrations.py
+++ db_migrations.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 DB_PATH = "image_editor.db"
-SCHEMA_VERSION = 4
+SCHEMA_VERSION = 7
 
 MIGRATIONS = [
     # version 1
@@ -70,6 +70,53 @@ MIGRATIONS = [
 
     PRAGMA foreign_keys = ON;
     UPDATE preferences SET value = '4' WHERE key = 'schema_version';
+    ''',
+    # version 5 - Insert thumbnail preferences defaults
+    '''
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_close_on_select', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_use_resample', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_size', '350', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_stretch_small', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_border', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_auto_scroll', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_sort_by_path', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_show_info', '1', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_show_common_shell', '1', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_warn_on_esc', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_try_exif', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_focus_tree_on_click', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_text_template', '$DSF $X\n$W x $H pixels\n$B bpp\n$S\n$T\n$E$E63667', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_mrud_count', '30', datetime('now', '-3 hours'));
+    UPDATE preferences SET value = '5' WHERE key = 'schema_version';
+    ''',
+    # version 6 - Save thumbnail window position/size preferences
+    '''
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_window_width', '1024', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_window_height', '768', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_window_x', '0', datetime('now', '-3 hours'));
+    INSERT OR IGNORE INTO preferences (key, value, criado_em) VALUES ('thumb_window_y', '0', datetime('now', '-3 hours'));
+    UPDATE preferences SET value = '6' WHERE key = 'schema_version';
+    ''',
+    # version 7 - add alterado_em column
+    '''
+    PRAGMA foreign_keys = OFF;
+
+    CREATE TABLE preferences_new (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        key TEXT UNIQUE,
+        value TEXT,
+        criado_em TIMESTAMP DEFAULT (datetime('now', '-3 hours')),
+        alterado_em TIMESTAMP DEFAULT (datetime('now', '-3 hours'))
+    );
+
+    INSERT INTO preferences_new (id, key, value, criado_em, alterado_em)
+    SELECT id, key, value, criado_em, datetime('now', '-3 hours') FROM preferences;
+
+    DROP TABLE preferences;
+    ALTER TABLE preferences_new RENAME TO preferences;
+
+    PRAGMA foreign_keys = ON;
+    UPDATE preferences SET value = '7' WHERE key = 'schema_version';
     '''
 ]
 
